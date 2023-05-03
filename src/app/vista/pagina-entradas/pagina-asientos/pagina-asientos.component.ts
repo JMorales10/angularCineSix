961822be1c7adc6b85406ea1b7700b89289d9a5f
Jesus Morales <jesusmoralescanete@gmail.com>
2023-05-03 20:15:52 +0200

    tabla asientos acabada

--- src/app/vista/pagina-entradas/pagina-asientos/pagina-asientos.component.ts
+++ src/app/vista/pagina-entradas/pagina-asientos/pagina-asientos.component.ts
@@ -1,13 +1,37 @@
 import { Component } from '@angular/core';
 
+export interface Asientos {
+  positionX: number;
+  positionY: number;
+  ocupado: Boolean;
+}
+
+let ELEMENT_DATA: any = []
+let ocupado : Boolean = true;
+let filaAsientos: any = [];
+
+  for(let x = 0; x < 10; x++){
+    for(let y = 0; y < 10; y++){
+      x % 2 == 0 ? ocupado = true : ocupado = false
+      console.log(ocupado)
+      const asiento: Asientos = {positionX: x, positionY: y, ocupado: ocupado};
+      filaAsientos.push({asiento: asiento, fila: x})
+    }
+    ELEMENT_DATA.push(filaAsientos)
+    filaAsientos = []
+  }
+
+  console.log(ELEMENT_DATA)
+
 @Component({
   selector: 'app-pagina-asientos',
   templateUrl: './pagina-asientos.component.html',
   styleUrls: ['./pagina-asientos.component.css']
 })
+
 export class PaginaAsientosComponent {
 
-  public columnas: any = 10
-  public filas: any = 10
+  displayedColumns: string[] = ['ocupado'];
+  dataSource = ELEMENT_DATA;
 
 }
