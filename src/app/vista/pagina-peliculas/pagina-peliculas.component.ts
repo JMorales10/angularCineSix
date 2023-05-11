79e91c30a958cf6437a5b9f095922297c12877ba
Jesus Morales <jesusmoralescanete@gmail.com>
2023-05-11 21:11:16 +0200

    imagenes arregladas

--- src/app/vista/pagina-peliculas/pagina-peliculas.component.ts
+++ src/app/vista/pagina-peliculas/pagina-peliculas.component.ts
@@ -1,5 +1,6 @@
 import { peliculaService } from 'src/app/shared/services/pelicula-service.service';
 import { Component, OnInit } from '@angular/core';
+import { IMAGES_URL } from 'src/constant'
 
 @Component({
   selector: 'app-pagina-peliculas',
@@ -7,6 +8,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./pagina-peliculas.component.css']
 })
 export class PaginaPeliculasComponent implements OnInit{
+  public images_url = IMAGES_URL;
   public listPeliculas: any;
 
   constructor(private filmService: peliculaService){}
